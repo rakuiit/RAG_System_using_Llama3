55bbf52dd5ad7bda81c81b6c368dcaecc55b286b
rakuiit <en.rakeshsharma@gmail.com>
2024-09-17 21:31:32 +0530

    Added post endpoint for Query

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,request
 from langchain_community.llms import Ollama
 
 app=Flask(__name__)
@@ -8,6 +8,17 @@ app=Flask(__name__)
 # response=llm.invoke("why is  the sky blue?")
 
 # print(response)
+
+@app.route("/ai",methods=["POST"])
+def aiPOST():
+    print("post ai called")
+    json_content=request.json
+    query=json_content.get("query")
+
+    print(f"query:{query}")
+    
+    response_answer=f"sample Response,query: {query}"
+    return response_answer
 
 
 if __name__=="__main__":
